d46f42452b65cfc14d0055f4b6a6b269cc1b6fc9
mario alberto <clcneogeek@gmail.com>
2014-11-14 23:52:05 -0600

    un poco mas de avance del prototipo

--- dj.sh
+++ dj.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
 
-echo $1
-echo $2
+
+
+
+case $1 in
+    "proyecto") echo -e ".\n..\n...\ncreando el proyecto django $2" ; 
+                django-admin.py startproject $2;;
+
+	"init" )  echo -e "\n Iniciando el proyecto......"
+              url=$(pwd)
+              export PATH="$url:$PATH"
+              echo "[$url] es la ruta del proyecto"
+            ;;
+
+	"com" ) echo "has agregado el siguente comentario $2";;
+
+	"addconfig" ) echo "creaando los directorios y las configuraciones necesarias";;
+
+	"" )  echo -e "\ndj.sh init | es para inicial un proyecto "
+               echo "dj.sh  proyecto nombre_del_proyecto | es para crear un proyecto"
+               echo "dj.sh com 'aqui va tu comentario'| esto es para agragar una notita al proyecto"
+               echo "dj.sh  cerrar | es para cerrar del proyecto"
+
+               ;;
+	"cerrar" )  echo "Cerrando el proyecto" && exit ;;
+esac
+
+
+
+
